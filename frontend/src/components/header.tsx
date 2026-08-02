dcfc2b3344ef3fc886c3532d089d8edf2a1e35ca
rob8624 <robert_melen@yahoo.co.uk>
2026-08-02 16:19:04 +0100

    added min contect section and sidebar

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -11,11 +11,11 @@ export default function Header() {
         <div className='flex flex-col items-center sm:flex-row sm:justify-between sm:min-h-30 '>
             <img className="sm:self-end mt-5" src={header.logo.formats?.thumbnail?.url}/>
              <nav className='sm:self-end'>
-                <ol className='flex gap-2'>
+                <ul className='flex gap-2'>
                     {header.menu.map(item => 
                         <li >{item.label}</li>
                     )}
-                </ol>
+                </ul>
             </nav>
         </div>
         <div className='h-5 bg-[#E0FAFF] rounded-2xl'></div>
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -6,11 +6,26 @@ export const Route = createFileRoute('/')({ component: Home })
 function Home() {
   return (
     
-      <div>
+      <main className='grid grid-cols-1 grid-rows-[auto_1fr] sm:grid-cols-[2fr_1fr] sm:grid-rows-1 pt-10  '>
         
-        <div>Content</div>
+        {/* artlices */}
+        <section className='order-2 sm:order-1  flex flex-col sm:flex-row gap-2'>
+          <div className='sm:flex-1 border-b-2 sm:border-b-0 sm:border-r-2'>
+            <h1 className='sm:mt-4 text-2xl font-albert font-bold'>Articles</h1>
+            <p className='font-cabin text-gray-500 italic'>To read, is to Learn</p>
+          </div>
+          <div className='sm:flex-4'>Post list</div>
+        </section>
         
-        </div>
+        
+        
+        {/* sidebar */}
+        <section className='order-1 sm:order-2 bg-blue-600 flex sm:flex-col'>
+          <div className='bg-yellow-200 flex-1'>Categories</div>
+          <div className='bg-yellow-700 flex-2'>Picture of the moment</div>
+        </section>
+        
+      </main>
       
     
   )
