173366f94dfeb8c8d6fcbe296cdaa17021efd111
rob8624 <robert_melen@yahoo.co.uk>
2026-09-25 22:21:14 +0100

    new styles on author info section

--- frontend/src/components/postSideBar.tsx
+++ frontend/src/components/postSideBar.tsx
@@ -45,33 +45,27 @@ export function PostSideBar({author, className, publishedDate, categories, updat
                 <div className=" lg:mt-50 md:border-t-2 border-b-2 border-t-2 border-gray-400 
                 flex flex-row lg:flex-col gap-5 w-full items-center lg:items-start  ">
                    
-                    
-                    <div className="flex flex-row lg:gap-1 border-r-2 lg:border-none p-4 lg:p-0 gap-1 items-center text-sm flex-wrap">
-                        <div className="flex flex-col">
-                                <div className="opacity-50 italic">Written by</div>
-                                <div className="font-cabin font-bold whitespace-nowrap ">{name} </div>
-                                <div className="italic text-gray-500">{position}</div>
+
+                    <div className="grid grid-cols-[auto_1fr] lg:grid-rows-2 lg:grid-cols-1  gap-4">
+                        <div className="border-r-2 lg:border-r-0 p-2">
+                            <div className="opacity-50 italic">Written by</div>
+                            <div className="font-cabin font-bold whitespace-nowrap ">{name} </div>
+                            <div className="italic text-gray-500">{position}</div>
                         </div>
-                        
-                    
-
-                    </div>
-
-                    <div className="flex flex-row lg:flex-col lg:gap-2 text-sm flex-wrap items-center lg:items-start h-fit">
-                        <div className="flex flex-row lg:flex-col gap-1 m-1 lg:gap-0 lg:m-0 ">
-                            <dt className="font-cabin font-bold">Published</dt>
+                        <div className="">
+                            <dt className="font-cabin font-bold text-xs">Published</dt>
                             <dd><time dateTime={publishedDate}>
-                                    {localDate}
-                                </time></dd>
-                        </div>
-                        <div className="flex flex-row  lg:flex-col gap-1 lg:gap-0 lg:m-0 ">
-                            <dt className="font-cabin font-bold">Updated</dt>
+                                {localDate}
+                            </time></dd>
+                            <dt className="font-cabin font-bold text-xs">Updated</dt>
                             <dd> <time dateTime={updatedDate}>
-                                    {dateUpdated}
-                                </time>
-                                </dd>
+                                {dateUpdated}
+                            </time>
+                            </dd>
                         </div>
+
                     </div>
+
                     
                 </div>
                 
@@ -79,6 +73,7 @@ export function PostSideBar({author, className, publishedDate, categories, updat
             <div className="mt-2">
                 <button className="p-1 bg-primary border-2 text-on-primary">Share</button>
             </div>
+
             
         </div>
     )
